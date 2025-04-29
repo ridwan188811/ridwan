ce2a83ad8a333ec9eb979cda6b6213c996d126bf
ridwan188811 <mamalemon193@gmail.com>
2025-04-29 15:04:46 +0700

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-// Tidak ada data tugas untuk saat ini
-// JavaScript ini disiapkan untuk update tugas di masa depan
-
-// Fungsi dasar untuk placeholder jika dibutuhkan nanti
-function showTaskMessage() {
-    alert('Belum ada tugas yang tersedia.');
-}
